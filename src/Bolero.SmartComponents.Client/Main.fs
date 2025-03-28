983711be091f271e739553f950138ba0c2944fde
Karel Honzl <karel.honzl@gmail.com>
2025-03-28 15:01:41 +0100

    Added smart paste

--- src/Bolero.SmartComponents.Client/Main.fs
+++ src/Bolero.SmartComponents.Client/Main.fs
@@ -8,24 +8,40 @@ open Bolero.Html
 open Bolero.Templating.Client
 open SmartComponents
 
+type Address ={
+        name:string
+        addr1:string
+        city:string
+        zip:string 
+}
+
 type Model =
     {
         x: string
+        address:Address
     }
 
 let initModel =
     {
         x = ""
+        address = {
+            addr1=""
+            city=""
+            name=""
+            zip=""
+        }
     }
 
 type Message =
     | Ping
     |ChangeData of string
+    |ChangeAddress of Address
 
 let update message model =
     match message with
     | Ping -> model
     | ChangeData v -> {model with x = v}
+    | ChangeAddress a -> {model with address = a}
 
     
 type Expr = 
@@ -33,12 +49,43 @@ type Expr =
 
 let view model dispatch =
     let l= Expr.Quote (fun () -> model.x)
-    div{
-        comp<SmartTextArea>{
-            "UserRole"=>"system"
-            "Value" => model.x
-            attr.callback<string> "ValueChanged" (ChangeData >> dispatch )
-            "ValueExpression" => l
+    concat{
+        div{
+            comp<SmartTextArea>{
+                "UserRole"=>"system"
+                "Value" => model.x
+                attr.callback<string> "ValueChanged" (ChangeData >> dispatch )
+                "ValueExpression" => l
+            }
+        }
+        form{
+            p {
+                "Name:" 
+                input {
+                    bind.input.string model.address.name (fun x-> dispatch (ChangeAddress {model.address with name = x}) )
+                }
+            }
+            p {
+                "Address line 1: " 
+                input {
+                    bind.input.string model.address.addr1 (fun x-> dispatch (ChangeAddress {model.address with addr1 = x}) )
+                }
+            }
+            p {
+                "City: " 
+                input {
+                    bind.input.string model.address.city (fun x-> dispatch (ChangeAddress {model.address with city = x}) ) 
+                }
+            }
+            p {
+                "Zip/postal code: " 
+                input {
+                    bind.input.string model.address.zip (fun x-> dispatch (ChangeAddress {model.address with zip = x}) ) 
+                }
+            }
+            comp<SmartPasteButton>{
+                "DefaultIcon"=> true
+            }
         }
     }
 
